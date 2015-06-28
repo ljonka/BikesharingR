d2a34c58db400a300a232e196d980b28cee233cd
Technologie <technologie@transition-regensburg.de>
2015-06-28 12:27:06 +0200

    backup

--- config/db.php
+++ config/db.php
@@ -5,6 +5,6 @@ return [
     #'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     #'username' => 'root',
     #'password' => '',
-    'dsn' => 'sqlite:/var/www/virtual/trwandel/priv_intern.bikesharing-r.de/data/data.db',
+    'dsn' => 'sqlite:/var/www/virtual/trwandel/bikesharing-r.de_yii/data/data.db',
     'charset' => 'utf8',
 ];
